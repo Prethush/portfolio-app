9fd16bbdfe6af2cb74220bbddbd2a7de0cd8c442
Prethush <ktmirash49@.gmail.com>
2021-10-27 16:47:49 +0530

    refactor

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,6 @@
 import React, {useState} from 'react'
 
 export default function Header() {
-  let [tag, setTag] = useState("home");
   let [isClicked, setClick] = useState(false);
 
   return (
@@ -15,20 +14,20 @@ export default function Header() {
         <input onClick={() => setClick(!isClicked)} type="checkbox" id="toggle" name="toggle" className="hidden"/>
         <nav className="nav">
             <ul className="flex nav-menu">
-              <li className={"mx-3 pb-2" + (tag ==="home" ? " border-b-4 border-blue-500 " : "")}>
-                <a href="#home" className="font-bold text-xl" onClick={() => setTag("home")}><h3>Home</h3></a>
+              <li className="mx-3">
+                <a href="#home" className="font-bold text-xl hover:text-blue-500"><h3>Home</h3></a>
               </li>
-              <li className={"mx-3 pb-2" + (tag ==="about" ? " border-b-4 border-blue-500" : "")}>
-                <a href="#about" className="font-bold text-xl" onClick={() => setTag("about")}><h3>About</h3></a>
+              <li className="mx-3">
+                <a href="#about" className="font-bold text-xl hover:text-blue-500"><h3>About</h3></a>
                 </li>
-              <li className={"mx-3 pb-2" + (tag ==="skills" ? " border-b-4 border-blue-500" : "")}>
-                <a href="#skills" className="font-bold text-xl" onClick={() => setTag("skills")}><h3>Skills</h3></a>
+              <li className="mx-3">
+                <a href="#skills" className="font-bold text-xl hover:text-blue-500"><h3>Skills</h3></a>
               </li>
-              <li className={"mx-3 pb-2" + (tag ==="projects" ? " border-b-4 border-blue-500" : "")}>
-                <a href="#projects" className="font-bold text-xl" onClick={() => setTag("projects")}><h3>Projects</h3></a>
+              <li className="mx-3">
+                <a href="#projects" className="font-bold text-xl hover:text-blue-500"><h3>Projects</h3></a>
               </li>
-              <li className={"mx-3 pb-2" + (tag ==="contact" ? " border-b-4 border-blue-500" : "")}>
-                <a href="#contact" className="font-bold text-xl" onClick={() => setTag("contact")}><h3>Contact</h3></a>
+              <li className="mx-3">
+                <a href="#contact" className="font-bold text-xl hover:text-blue-500"><h3>Contact</h3></a>
               </li>
             </ul>
         </nav>
